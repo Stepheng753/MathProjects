57936d8767421e617897cf1af7b204420cf25081
Stephen Giang <stepheng753@gmail.com>
2022-03-17 09:14:06 -0700

    Cleaned up code

--- CountDown Numbers/Development-Testing/Evaluate.py
+++ CountDown Numbers/Development-Testing/Evaluate.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import itertools
 import math
 import csv
@@ -7,33 +9,39 @@ import time
 class Solver:
 
     def __init__(self, target = 0, num_large = 4, num_total=6):
-        self.target = target
-        self.num_large = num_large
-        self.num_total = num_total
-        self.small_numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9]
-        self.large_numbers = [25, 50, 75, 100] 
-        self.numbers = random.sample(self.small_numbers, self.num_total - self.num_large) + \
-                        random.sample(self.large_numbers, self.num_large)
-        self.operations = ['+', '-', 'x', '/']
-        self.expression_arrs = []
-        self.solution_arrs = []
+        self.__target = float(target)
+        self.__num_large = num_large
+        self.__num_total = num_total
+        self.__small_numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9]
+        self.__large_numbers = [25, 50, 75, 100] 
+        self.__numbers = random.sample(self.__small_numbers, self.__num_total - self.__num_large) + \
+                        random.sample(self.__large_numbers, self.__num_large)
+        self.__operations = ['+', '-', 'x', '/']
+        self.__expression_arrs = []
+        self.__solution_arrs = []
+
+    def get_numbers(self):
+        return self.__numbers
 
     def set_numbers(self, numbers):
-        self.numbers = numbers
+        self.__numbers = numbers
+
+    def get_target(self):
+        return self.__target
     
-    def get_permutations(self, num_total):
+    def __get_permutations(self, num_total):
         number_arrs = []
-        for perm in list(itertools.permutations(self.numbers, num_total)):
+        for perm in list(itertools.permutations(self.__numbers, num_total)):
             number_arrs.append(perm)
         return number_arrs
 
-    def get_product(self, num_total):
+    def __get_product(self, num_total):
         operation_arrs = []
-        for prod in list(itertools.product(self.operations, repeat = num_total - 1)):
+        for prod in list(itertools.product(self.__operations, repeat = num_total - 1)):
             operation_arrs.append(prod)
         return operation_arrs
 
-    def get_expressions_wo_parens(self, number_arrs, operation_arrs):
+    def __get_expressions_wo_parens(self, number_arrs, operation_arrs):
         for num_seq in number_arrs:
             for op_seq in operation_arrs:
                 expression = []
@@ -41,9 +49,9 @@ class Solver:
                     expression.append(num_seq[i])
                     expression.append(op_seq[i])
                 expression.append(num_seq[len(num_seq) - 1])
-                self.expression_arrs.append(expression)
+                self.__expression_arrs.append(expression)
 
-    def make_parentheses(self, expression, top_level=False):
+    def __make_parentheses(self, expression, top_level=False):
         if len(expression) > 3:
             num_groups =  math.floor(len(expression) / 2) - 1 
             exp_paren = []
@@ -53,14 +61,14 @@ class Solver:
                 post_right_parenthesis = pre_left_parenthesis + paren_length
 
                 check1 = (post_right_parenthesis < len(expression) and pre_left_parenthesis == 0 and \
-                        self.is_opposite_operators(expression[post_right_parenthesis - 2], expression[post_right_parenthesis])) or \
+                        self.__is_opposite_operators(expression[post_right_parenthesis - 2], expression[post_right_parenthesis])) or \
                         \
                         (pre_left_parenthesis > 0 and post_right_parenthesis == len(expression) and \
-                        self.is_opposite_operators(expression[pre_left_parenthesis - 1], expression[pre_left_parenthesis + 1])) or \
+                        self.__is_opposite_operators(expression[pre_left_parenthesis - 1], expression[pre_left_parenthesis + 1])) or \
                         \
                         (pre_left_parenthesis > 0 and post_right_parenthesis < len(expression) and \
-                        self.is_opposite_operators(expression[post_right_parenthesis - 2], expression[post_right_parenthesis]) and \
-                        self.is_opposite_operators(expression[pre_left_parenthesis - 1], expression[pre_left_parenthesis + 1]))
+                        self.__is_opposite_operators(expression[post_right_parenthesis - 2], expression[post_right_parenthesis]) and \
+                        self.__is_opposite_operators(expression[pre_left_parenthesis - 1], expression[pre_left_parenthesis + 1]))
 
                 check2 = post_right_parenthesis <= len(expression)
 
@@ -72,16 +80,16 @@ class Solver:
                         
                         if len(left[0]) >= 3:
                             right_op = left[0][-1]
-                            left = self.make_parentheses(left[0][0:-1])
+                            left = self.__make_parentheses(left[0][0:-1])
                             for item in left:
                                 item.append(right_op)
                         if len(right[0]) >= 3:
                             left_op = right[0][0]
-                            right = self.make_parentheses(right[0][1:])
+                            right = self.__make_parentheses(right[0][1:])
                             for item in right:
                                 item.insert(0, left_op)
 
-                        middle = self.make_parentheses(middle)
+                        middle = self.__make_parentheses(middle)
 
                         for litem in left:
                             for mitem in middle:
@@ -98,14 +106,14 @@ class Solver:
                     post_right_parenthesis = pre_left_parenthesis + paren_length
 
                     check1 = (post_right_parenthesis < len(expression) and pre_left_parenthesis == 0 and \
-                            self.is_opposite_operators(expression[post_right_parenthesis - 2], expression[post_right_parenthesis])) or \
+                            self.__is_opposite_operators(expression[post_right_parenthesis - 2], expression[post_right_parenthesis])) or \
                             \
                             (pre_left_parenthesis > 0 and post_right_parenthesis == len(expression) and \
-                            self.is_opposite_operators(expression[pre_left_parenthesis - 1], expression[pre_left_parenthesis + 1])) or \
+                            self.__is_opposite_operators(expression[pre_left_parenthesis - 1], expression[pre_left_parenthesis + 1])) or \
                             \
                             (pre_left_parenthesis > 0 and post_right_parenthesis < len(expression) and \
-                            self.is_opposite_operators(expression[post_right_parenthesis - 2], expression[post_right_parenthesis]) and \
-                            self.is_opposite_operators(expression[pre_left_parenthesis - 1], expression[pre_left_parenthesis + 1]))
+                            self.__is_opposite_operators(expression[post_right_parenthesis - 2], expression[post_right_parenthesis]) and \
+                            self.__is_opposite_operators(expression[pre_left_parenthesis - 1], expression[pre_left_parenthesis + 1]))
 
                     check2 = post_right_parenthesis <= len(expression)
 
@@ -114,23 +122,24 @@ class Solver:
         else:
             return [["("] + expression + [")"]] if not top_level else [expression]
 
-    def is_opposite_operators(self, element1, element2):
+    def __is_opposite_operators(self, element1, element2):
         return (element1 == "+" and element2 == "x") or (element1 == "+" and element2 == "/") or \
                 (element1 == "-" and element2 == "x") or (element1 == "-" and element2 == "/") or \
                 (element1 == "x" and element2 == "+") or (element1 == "x" and element2 == "-") or \
                 (element1 == "/" and element2 == "+") or (element1 == "/" and element2 == "-")
 
-    def get_all_expressions(self):
-        self.get_expressions_wo_parens(self.get_permutations(self.num_total), self.get_product(self.num_total))
+    def get_all_expressions(self, start=2):
+        for i in range(start, self.__num_total + 1):
+            self.__get_expressions_wo_parens(self.__get_permutations(i), self.__get_product(i))
 
         every_expression = []
-        for expwop in self.expression_arrs:
-            for exp in self.make_parentheses(expwop, True):
+        for expwop in self.__expression_arrs:
+            for exp in self.__make_parentheses(expwop, True):
                 every_expression.append(exp)
 
-        self.expression_arrs = every_expression
+        self.__expression_arrs = every_expression
 
-        return self.expression_arrs
+        return self.__expression_arrs
 
     def simplify_expression(self, expression):
         left_parenthesis_index = -1
@@ -192,16 +201,19 @@ class Solver:
         return sum
 
     def get_solutions(self):
-        for exp in self.expression_arrs:
-            if self.simplify_expression(exp) == self.target:
-                self.solution_arrs.append(exp)
+        if len(self.__expression_arrs) == 0:
+            self.get_all_expressions()
+        for exp in self.__expression_arrs:
+            if self.simplify_expression(exp) == self.__target:
+                self.__solution_arrs.append(exp)
+
+        return self.__solution_arrs
 
     def __str__(self):
         rtnStr = ""
-        rtnStr = rtnStr + "Target: " + str(self.target) + "\n"
-        rtnStr = rtnStr + "Numbers: " + str(self.numbers)
+        rtnStr = rtnStr + "Target: " + str(self.__target) + "\n"
+        rtnStr = rtnStr + "Numbers: " + str(self.__numbers)
         return rtnStr
-
 
 
 def printArr(arr):
@@ -210,51 +222,77 @@ def printArr(arr):
         rtnStr = rtnStr + str(item) + " "
     return rtnStr
 
+def replace(arr, element1, element2):
+    for i in range(0, len(arr)):
+        if (arr[i] == element1):
+            arr[i] = element2
+    return arr
 
 def printAll():
+    start = time.perf_counter()
     counter = 0
     letters = ['A', 'B', 'C', 'D', 'E', 'F']
-    for i in range(2, 7):
-        s = Solver(0, 0, i)
-        s.set_numbers(letters[0: i + 1])
-        expressions = s.get_all_expressions()
-        for exp in expressions:
-            print(str(counter) + ": " + printArr(exp) + " = " + str(s.simplify_expression(exp)))
-            counter = counter + 1  
+    s = Solver(0, 0, 6)
+    s.set_numbers(letters)
+    expressions = s.get_all_expressions()
+    for exp in expressions:
+        print(str(counter) + ": " + printArr(exp) + " = " + str(s.simplify_expression(exp)))
+        counter = counter + 1  
+        print("Time: " + str(round(time.perf_counter() - start, 4)) + " secs", end='\r')
+    print("Time: " + str(round(time.perf_counter() - start, 4)) + " secs")
+    
+
+def find_solutions():
+    start = time.perf_counter()
+    counter = 0
+    s = Solver(random.randint(100, 999), random.randint(0, 4))
+    print(s)
+    solutions = s.get_solutions()
+    for sol in solutions:
+        print(str(counter) + ": ", printArr(sol) + " = " + str(s.get_target()))
+        counter = counter + 1
+        print("Time: " + str(round(time.perf_counter() - start, 4)) + " secs", end='\r')
+    print("Time: " + str(round(time.perf_counter() - start, 4)) + " secs")
+     
 
 
 def writeCSV():
+    start = time.perf_counter()
     with open('all_perms.csv', "w") as csvfile:
         csvwriter = csv.writer(csvfile)
         counter = 0
         letters = ['A', 'B', 'C', 'D', 'E', 'F']
-        for i in range(2, 7):
-            s = Solver(0, 0, i)
-            s.set_numbers(letters[0: i + 1])
-            expressions = s.get_all_expressions()
-            for exp in expressions:
-                csvwriter.writerow([counter] + exp)
-                print(str(counter) + ": " + printArr(exp))
-                counter = counter + 1
+        s = Solver(0, 0, 6)
+        s.set_numbers(letters)
+        expressions = s.get_all_expressions()
+        for exp in expressions:
+            csvwriter.writerow([counter] + exp)
+            print(str(counter) + ": " + printArr(exp))
+            counter = counter + 1
+            print("Time: " + str(round(time.perf_counter() - start, 4)) + " secs", end='\r')
+    print("Time: " + str(round(time.perf_counter() - start, 4)) + " secs")
         
 
 def readCSV():
+    start = time.perf_counter()
     s = Solver()
     with open("all_perms.csv", "r") as csvfile:
         csvreader = csv.reader(csvfile)
-        i = 0
+        counter = 0
         letters = ['A', 'B', 'C', 'D', 'E', 'F']
         for line in csvreader:
             newLine = line
             for i in range(0, len(letters)):
-                newLine = replace(newLine, letters[i], s.numbers[i])
+                newLine = replace(newLine, letters[i], s.get_numbers()[i])
             newLine = newLine[1 : ]
             solution = s.simplify_expression(newLine)
-            print(str(i) + ": ", printArr(newLine) + " = " + str(solution))
-            i = i + 1
+            print(str(counter) + ": ", printArr(newLine) + " = " + str(solution))
+            counter = counter + 1
+            print("Time: " + str(round(time.perf_counter() - start, 4)) + " secs", end='\r')
+    print("Time: " + str(round(time.perf_counter() - start, 4)) + " secs")
 
 
-def find_solutions():
+def find_solutions_csv():
     start = time.perf_counter()
     s = Solver(random.randint(100, 999), random.randint(0, 4))
     print(s)
@@ -265,25 +303,20 @@ def find_solutions():
         for line in csvreader:
             newLine = line
             for i in range(0, len(letters)):
-                newLine = replace(newLine, letters[i], s.numbers[i])
+                newLine = replace(newLine, letters[i], s.get_numbers()[i])
             newLine = newLine[1 : ]
-
             solution = s.simplify_expression(newLine)
-            if (s.target == solution):
+
+            if (solution == s.get_target()):
                 print(str(counter) + ": ", printArr(newLine) + " = " + str(solution))
             counter = counter + 1
             print("Time: " + str(round(time.perf_counter() - start, 4)) + " secs", end='\r')
-
-def replace(arr, element1, element2):
-    for i in range(0, len(arr)):
-        if (arr[i] == element1):
-            arr[i] = element2
-    return arr
-
+    print("Time: " + str(round(time.perf_counter() - start, 4)) + " secs")
 
 if __name__ == '__main__':
 
     # writeCSV()
     # printAll()
+    # find_solutions()
     # readCSV()
-    find_solutions()
+    find_solutions_csv()
